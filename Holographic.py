9b02412e4cd5b02c19a8d567119f3b530c0af4ee
calvin <504461251@qq.com>
2018-03-29 13:31:11 +0800

    删除Holographic2.py 修改Holographic.py

--- Holographic.py
+++ Holographic.py
@@ -1,31 +1,40 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import function
 from numba import jit
 @jit
 
-def run_Holographic(col, raw, r0, n):
+def run_Holographic(col, raw):
+	wavelength = 0.78e-6
+	f = 5e-6
+	k = 2 * np.pi / wavelength
+	n = 2
+	r0 = 1.2e-6
+	inter = 1.2e-6
 
-	x = np.linspace(-col/200, col/200, col)
-	y = np.linspace(-raw/200, raw/200, raw)
+	x = np.linspace(-5e-6, 5e-6, col)
+	y = np.linspace(-5e-6, 5e-6, raw)
+
 	[xv,yv]=np.meshgrid(x,y)
 	r = np.zeros((raw,col))
 	theta = np.zeros((raw,col))
 	Tn = np.zeros((raw,col))
 
+	r = np.sqrt(xv**2+yv**2)
 	for i in range(0,raw):
 		for j in range(0,col):
-			r[i][j] = np.sqrt(xv[i][j]**2+yv[i][j]**2)
 			theta[i][j] = np.arctan2(yv[i][j],xv[i][j])
 			if yv[i][j]<0:
-				theta[i][j] +=2*np.pi
+				theta[i][j] += 2*np.pi
 
-	for i in range(0,raw):
-		for j in range(0,col):
-			Tn[i][j] = n*theta[i][j]-2*np.pi*r[i][j]/r0
-			while Tn[i][j]<0:
-				Tn[i][j] += 2*np.pi
-			while Tn[i][j]>2*np.pi:
-				Tn[i][j] -= 2*np.pi
+	# n阶的相位板
+	Tn = function.fun1(raw, col, theta, Tn, n)
+	# 薄透镜
+	#Tn = function.fun2(raw, col, theta, r, Tn, k, f)
+    # 薄透镜+n阶的相位板
+	#Tn = function.fun3(raw, col, theta, r, Tn, n, k, f)
+	# n阶的相位板+贝塞尔
+	#Tn = function.fun4(raw, col, theta, r, Tn, n, r0)
 
 	# np.savetxt("r.dat", r, fmt='%.18e', delimiter=' ', newline='\n')
 	# np.savetxt("theta.dat", theta, fmt='%.18e', delimiter=' ', newline='\n')
@@ -34,7 +43,7 @@ def run_Holographic(col, raw, r0, n):
 	plt.axis("equal")
 	plt.imshow(Tn, cmap=plt.cm.gray) #灰度
 	plt.colorbar() 
-	plt.savefig("test2.png")
+	plt.savefig("test.png")
 	plt.show()
 
-run_Holographic(col=4000,raw=4000, r0=5, n=2)
+run_Holographic(col=2000, raw=2000)
--- function.py
+++ function.py
@@ -4,42 +4,31 @@ from numba import jit
 
 def fun1(raw, col, theta, Tn, n):
 	# n阶的相位板
-	for i in range(0,raw):
-		for j in range(0,col):
-			Tn[i][j] = n*theta[i][j]
-			while Tn[i][j]<0:
-				Tn[i][j] += 2*np.pi
-			while Tn[i][j]>2*np.pi:
-				Tn[i][j] -= 2*np.pi
+	Tn = n*theta
+	Tn = too(raw,col,Tn)
 	return Tn
 
 def fun2(raw, col, theta, r, Tn, k, f):
 	# 薄透镜
-	for i in range(0,raw):
-		for j in range(0,col):
-			Tn[i][j] = k*(r[i][j]**2)/2/f
-			while Tn[i][j]<0:
-				Tn[i][j] += 2*np.pi
-			while Tn[i][j]>2*np.pi:
-				Tn[i][j] -= 2*np.pi
+	Tn = k*(r**2)/2/f
+	Tn = too(raw,col,Tn)
 	return Tn
 
 def fun3(raw, col, theta, r, Tn, n, k, f):
 	# 薄透镜+n阶的相位板
-	for i in range(0,raw):
-		for j in range(0,col):
-			Tn[i][j] = n*theta[i][j] + k*(r[i][j]**2)/2/f
-			while Tn[i][j]<0:
-				Tn[i][j] += 2*np.pi
-			while Tn[i][j]>2*np.pi:
-				Tn[i][j] -= 2*np.pi
+	Tn = n*theta + k*(r**2)/2/f
+	Tn = too(raw,col,Tn)
 	return Tn
 
 def fun4(raw, col, theta, r, Tn, n, r0):
 	# n阶的相位板+贝塞尔
+	Tn = n * theta - 2*np.pi*r/r0
+	Tn = too(raw,col,Tn)
+	return Tn
+
+def too(raw,col,Tn):
 	for i in range(0,raw):
 		for j in range(0,col):
-			Tn[i][j] = n*theta[i][j] - 2*np.pi*r[i][j]/r0
 			while Tn[i][j]<0:
 				Tn[i][j] += 2*np.pi
 			while Tn[i][j]>2*np.pi:
